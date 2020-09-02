486bd4fc2a59d698689df478ebaeb1178e5ac09f
JJ Style <style.jj@pm.me>
2020-09-02 17:17:07 +0100

    create game form with standard library

--- chipz-app/components/CreateForm.js
+++ chipz-app/components/CreateForm.js
@@ -1,10 +1,93 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, Switch, Slider, TouchableHighlight } from 'react-native';
+
+const styles = StyleSheet.create({
+    fieldContainer: {
+        flex: 1,
+        marginTop: 20,
+        marginBottom: 20,
+        marginRight: 10,
+        marginLeft: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    text: {
+        height: 40,
+        width: 130,
+        margin: 0,
+        marginRight: 7,
+        paddingLeft: 10
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18
+    },
+});
 
 export const CreateForm = () => {
+
+    const [startingChips, setStartingChips] = useState(0);
+    const [useBlinds, setUseBlinds] = useState(true);
+    const [startingBlinds, setStartingBlinds] = useState(0);
+    const [blindInterval, setBlindInterval] = useState(15);
+
     return (
-        <View>
-            <Text>Create game form</Text>
+        <View style={{flex: 1}}>
+            <View style={styles.fieldContainer}>
+                <TextInput
+                    style={styles.text}
+                    keyboardType="numeric"
+                    placeholder="Starting chips"
+                    spellCheck={false}
+                    value={startingChips}
+                    onChangeText={(e) => setStartingChips(Number(e))}
+                    underlineColorAndroid="#48BBEC"
+                />
+                <View style={{flexDirection:'row', alignItems:'stretch'}}>
+                    <Text style={{marginRight: 30}}>Blinds</Text>
+                    <Switch
+                        trackColor={{ false: "maroon", true: "darkgreen" }}
+                        thumbColor={useBlinds ? "green" : "red"}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={() => setUseBlinds(!useBlinds)}
+                        value={useBlinds}
+                    />
+                </View>
+                <Text>Starting Blinds: £{startingBlinds}</Text>
+                <Slider 
+                    disabled={!useBlinds}
+                    minimumValue={0}
+                    maximumValue={startingChips || 1000}
+                    step={10}
+                    onValueChange={(n) => setStartingBlinds(n)}
+                    value={startingBlinds}
+                    style={{width: "75%"}}
+                />
+                <Text>Blind interval: {blindInterval} minutes {!blindInterval?"(Blinds won't increase)":""}</Text>
+                <Slider 
+                    disabled={!useBlinds}
+                    minimumValue={0}
+                    maximumValue={60}
+                    step={5}
+                    onValueChange={(n) => setBlindInterval(n)}
+                    value={blindInterval}
+                    style={{width: "75%"}}
+                />
+            </View>
+            <TouchableHighlight style={styles.button} onPress={() => console.log("Starting Game")}>
+                <Text style={styles.buttonText}>Start Game</Text>
+            </TouchableHighlight>
         </View>
     );
 }
--- chipz-app/components/HomeScreen.js
+++ chipz-app/components/HomeScreen.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, Image } from 'react-native';
 
-export const HomeScreen = () => {
+const styles = StyleSheet.create({
+    logo: {
+        width: "75%",
+        height: "75%",
+        alignSelf: 'center'
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18
+    },
+});
+
+export const HomeScreen = ({navigation}) => {
     return (
         <View>
-            <Text>Home Screen</Text>
+            <View>
+                <Image style={styles.logo} source={require("../assets/splash.png")} />
+                <TouchableHighlight style={styles.button} onPress={() => navigation.navigate("Create Game")}>
+                    <Text style={styles.buttonText}>Create Game</Text>
+                </TouchableHighlight>
+                <TouchableHighlight style={styles.button} onPress={() => navigation.navigate("Join Game")}>
+                    <Text style={styles.buttonText}>Join Game</Text>
+                </TouchableHighlight>
+            </View>
         </View>
     );
     
