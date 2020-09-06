79653ec2cb597cf5d44fb55abe3031076f9fd308
JJ Style <style.jj@pm.me>
2020-09-06 18:14:24 +0100

    display some info at top

--- chipz-app/components/GameScreen.js
+++ chipz-app/components/GameScreen.js
@@ -13,10 +13,8 @@ const tmpState = {
 
 const styles = StyleSheet.create({
     buttonGroup: {
-        flex: 1,
-        margin: 10,
         alignItems: 'center',
-        justifyContent: 'space-evenly',
+        justifyContent: 'space-around',
     },
     betButton: {
         backgroundColor: 'white',
@@ -24,19 +22,27 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 10,
         alignSelf: 'stretch',
-        margin: 10,
+        margin: 20,
         justifyContent: 'center',
         alignItems: 'center',
+        paddingTop: 10
     },
     quickBetRow: {
         flexDirection: 'row',
     },
     bigButton: {
-        height: "20%",
+        height: "25%",
     },
     bigText: {
         fontSize: 32,
         fontWeight: 'bold'
+    },
+    infoBox: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        alignSelf: 'center',
+        width: "100%",
+        borderRadius: 10
     }
 });
 
@@ -47,67 +53,74 @@ export const GameScreen = ({navigation}) => {
     //TODO: Reset navigation so can't go back
 
     return (
-        <View style={styles.buttonGroup}>
-            <StyledButton 
-                buttonText="Fold"
-                onPress={() => console.log("fold")}
-                style={styles.bigButton}
-                textStyle={styles.bigText}
-            />
-            {tmpState.lastBet === null ?
-                <StyledButton
-                    buttonText="Check"
-                    onPress={() => console.log("check")}
-                    style={styles.bigButton}
-                    textStyle={styles.bigText}    
-                />
-            :
-                <StyledButton
-                    buttonText="Call"
-                    onPress={() => console.log("call")}
+        <View style={{flex: 1, margin: 10, marginTop: 30}}>
+            <View style={styles.infoBox}>
+                <Text>Chips: £{tmpState.playersChips}</Text>
+                <Text>Pot: £{tmpState.pot}</Text>
+                <Text>Blinds: £{tmpState.smallBlind}/£{tmpState.smallBlind*2}</Text>
+            </View>
+            <View style={styles.buttonGroup}>
+                <StyledButton 
+                    buttonText="Fold"
+                    onPress={() => console.log("fold")}
                     style={styles.bigButton}
                     textStyle={styles.bigText}
                 />
-            }
-            <TouchableHighlight onPress={() => console.log("bet")} style={[styles.betButton, styles.bigButton]} underlayColor="#e6e6e6">
-                <View style={{width: "100%", alignItems: 'center'}}>
-                    <Text style={{fontSize: 18}}>Bet: £{newBet}</Text>
-                    <Slider 
-                        minimumValue={minBet}
-                        maximumValue={tmpState.playersChips}
-                        step={minBet}
-                        onValueChange={(n) => setNewBet(n)}
-                        value={newBet}
-                        style={{width: "75%", marginTop: 10, marginBottom: 10}}
+                {tmpState.lastBet === null ?
+                    <StyledButton
+                        buttonText="Check"
+                        onPress={() => console.log("check")}
+                        style={styles.bigButton}
+                        textStyle={styles.bigText}    
                     />
-                    <View style={styles.quickBetRow}>
-                        <StyledButton 
-                            buttonText = "Min"
-                            onPress={() => {setNewBet(minBet)}} 
-                            style={{width: "20%"}} 
-                            textStyle={{fontWeight: "bold"}}
+                :
+                    <StyledButton
+                        buttonText="Call"
+                        onPress={() => console.log("call")}
+                        style={styles.bigButton}
+                        textStyle={styles.bigText}
+                    />
+                }
+                <TouchableHighlight onPress={() => console.log("bet")} style={[styles.betButton, styles.bigButton]} underlayColor="#e6e6e6">
+                    <View style={{width: "100%", alignItems: 'center'}}>
+                        <Text style={{fontSize: 18}}>Bet: £{newBet}</Text>
+                        <Slider 
+                            minimumValue={minBet}
+                            maximumValue={tmpState.playersChips}
+                            step={minBet}
+                            onValueChange={(n) => setNewBet(n)}
+                            value={newBet}
+                            style={{width: "75%", marginTop: 10, marginBottom: 10}}
                         />
-                        <StyledButton
-                            buttonText = "1/2 Pot"
-                            onPress={() => {setNewBet( tmpState.pot/2 <= tmpState.playersChips ? tmpState.pot/2 : tmpState.playersChips)}}
-                            style={{width: "20%"}}
-                            textStyle={{fontWeight: "bold"}}
-                        />
-                        <StyledButton
-                            buttonText = "Pot"
-                            onPress={() => {setNewBet(tmpState.pot <= tmpState.playersChips ? tmpState.pot : tmpState.playersChips)}} 
-                            style={{width:"20%"}}
-                            textStyle={{fontWeight: "bold"}}
-                        />
-                        <StyledButton
-                            buttonText = "Max"
-                            onPress={() => {setNewBet(tmpState.playersChips)}} 
-                            style={{width:"20%"}}
-                            textStyle={{fontWeight: "bold"}}
-                        />
+                        <View style={styles.quickBetRow}>
+                            <StyledButton 
+                                buttonText = "Min"
+                                onPress={() => {setNewBet(minBet)}} 
+                                style={{width: "20%"}} 
+                                textStyle={{fontWeight: "bold"}}
+                            />
+                            <StyledButton
+                                buttonText = "1/2 Pot"
+                                onPress={() => {setNewBet( tmpState.pot/2 <= tmpState.playersChips ? tmpState.pot/2 : tmpState.playersChips)}}
+                                style={{width: "20%"}}
+                                textStyle={{fontWeight: "bold"}}
+                            />
+                            <StyledButton
+                                buttonText = "Pot"
+                                onPress={() => {setNewBet(tmpState.pot <= tmpState.playersChips ? tmpState.pot : tmpState.playersChips)}} 
+                                style={{width:"20%"}}
+                                textStyle={{fontWeight: "bold"}}
+                            />
+                            <StyledButton
+                                buttonText = "Max"
+                                onPress={() => {setNewBet(tmpState.playersChips)}} 
+                                style={{width:"20%"}}
+                                textStyle={{fontWeight: "bold"}}
+                            />
+                        </View>
                     </View>
-                </View>
-            </TouchableHighlight>
+                </TouchableHighlight>
+            </View>
         </View>
     );
 }
